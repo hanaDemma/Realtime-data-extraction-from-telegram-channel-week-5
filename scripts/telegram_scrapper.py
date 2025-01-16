54bc6862d6231d01b5c2d317701b2d6dc3e7a3a3
hana demma <hana.demma@gmail.com>
2025-01-16 16:25:41 +0300

    start scrapping

--- scripts/telegram_scrapper.py
+++ scripts/telegram_scrapper.py
@@ -15,17 +15,18 @@ api_id = os.getenv('TG_API_ID')
 api_hash = os.getenv('TG_API_HASH')
 
 # Initialize the Telegram client
-client = TelegramClient('scraping_session', api_id, api_hash)
+client = TelegramClient('new_scraping_session', api_id, api_hash)
 
 # Define directories for saving data
 csv_file = 'telegram_data.csv'
 image_folder = 'telegram_images'
 document_folder = 'telegram_documents'
+# os.makedirs(csv_file, exist_ok=True) 
 os.makedirs(image_folder, exist_ok=True)  # Create folder if it doesn't exist
 os.makedirs(document_folder, exist_ok=True)
 
   # List of channels to scrape
-channels = [
+channel_username = [
             '@ZemenExpress',
             '@nevacomputer', 
             '@helloomarketethiopia',
@@ -63,24 +64,31 @@ async def scrape_telegram_channels(channel):
         entity = await client.get_entity(channel_username)
         channel_title = entity.title
         print(f"Scraping historical data from {channel_username} ({channel_title})...")
-        async for message in client.iter_messages(entity, limit=400):
+        async for message in client.iter_messages(entity, limit=200):
             image_file = ''  # Initialize empty image filename
-
+            document_file = ''   # Initialize empty document filename
             # Download media (images) if available
-            if message.media:
+            if message.photo:  # Check for images (photos only)
                 image_file = await message.download_media(file=image_folder)
+
+              # Download documents if available
+            if message.document and not message.photo:  # If there's a document but not a photo
+               document_file = await message.download_media(file=image_folder)
+
             if message.message:
                 amharic_reg = r'[\u1200-\u137F0-9\+\-_]+'
                 amharic_text = ' '.join(re.findall(amharic_reg, message.message))
 
-                if amharic_text.strip():  # Only write rows with Amharic content
+                if amharic_text.strip()  or image_file:  # Only write rows with Amharic content
                     message_date = message.date.strftime('%Y-%m-%d %H:%M:%S') if message.date else '[No Date]'
                     sender_id = message.sender_id if message.sender_id else '[No Sender ID]'
-                    write_to_csv(message_date, sender_id, message.id, amharic_text)
+                    write_to_csv(message_date, sender_id, message.id, amharic_text,image_file,document_file)
     
         print(f"Finished scraping {channel_username}")
     print("Listening for real-time messages...")
     client.run_until_disconnected() 
+
+
 # Real-time message handler to update the CSV file when new messages arrive
 @client.on(events.NewMessage(chats=channel_username)) 
 async def real_time_message_handler(event):
